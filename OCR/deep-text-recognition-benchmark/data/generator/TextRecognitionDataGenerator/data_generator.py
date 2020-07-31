57ee08e2c6d66eaf87e82ec850668927e2dd0d5e
korea7030 <korea7030@naver.com>
2020-07-31 22:18:26 +0900

    generate korean language image for training

--- OCR/deep-text-recognition-benchmark/data/generator/TextRecognitionDataGenerator/data_generator.py
+++ OCR/deep-text-recognition-benchmark/data/generator/TextRecognitionDataGenerator/data_generator.py
@@ -3,12 +3,13 @@ import random as rnd
 
 from PIL import Image, ImageFilter
 
-from trdg import computer_text_generator, background_generator, distorsion_generator
-
+import computer_text_generator
+import background_generator
+import distorsion_generator
 try:
-    from trdg import handwritten_text_generator
+    import handwritten_text_generator
 except ImportError as e:
-    print("Missing modules for handwritten text generation.")
+    print('Missing modules for handwritten text generation.')
 
 
 class FakeTextDataGenerator(object):
@@ -21,35 +22,7 @@ class FakeTextDataGenerator(object):
         cls.generate(*t)
 
     @classmethod
-    def generate(
-        cls,
-        index,
-        text,
-        font,
-        out_dir,
-        size,
-        extension,
-        skewing_angle,
-        random_skew,
-        blur,
-        random_blur,
-        background_type,
-        distorsion_type,
-        distorsion_orientation,
-        is_handwritten,
-        name_format,
-        width,
-        alignment,
-        text_color,
-        orientation,
-        space_width,
-        character_spacing,
-        margins,
-        fit,
-        output_mask,
-        word_split,
-        image_dir,
-    ):
+    def generate(cls, index, text, font, out_dir, size, extension, skewing_angle, random_skew, blur, random_blur, background_type, distorsion_type, distorsion_orientation, is_handwritten, name_format, width, alignment, text_color, orientation, space_width, margins, fit):
         image = None
 
         margin_top, margin_left, margin_bottom, margin_right = margins
@@ -62,55 +35,36 @@ class FakeTextDataGenerator(object):
         if is_handwritten:
             if orientation == 1:
                 raise ValueError("Vertical handwritten text is unavailable")
-            image, mask = handwritten_text_generator.generate(text, text_color)
+            image = handwritten_text_generator.generate(text, text_color, fit)
         else:
-            image, mask = computer_text_generator.generate(
-                text,
-                font,
-                text_color,
-                size,
-                orientation,
-                space_width,
-                character_spacing,
-                fit,
-                word_split,
-            )
-        random_angle = rnd.randint(0 - skewing_angle, skewing_angle)
+            image = computer_text_generator.generate(text, font, text_color, size, orientation, space_width, fit)
 
-        rotated_img = image.rotate(
-            skewing_angle if not random_skew else random_angle, expand=1
-        )
+        random_angle = rnd.randint(0-skewing_angle, skewing_angle)
 
-        rotated_mask = mask.rotate(
-            skewing_angle if not random_skew else random_angle, expand=1
-        )
+        rotated_img = image.rotate(skewing_angle if not random_skew else random_angle, expand=1)
 
         #############################
         # Apply distorsion to image #
         #############################
         if distorsion_type == 0:
-            distorted_img = rotated_img  # Mind = blown
-            distorted_mask = rotated_mask
+            distorted_img = rotated_img # Mind = blown
         elif distorsion_type == 1:
-            distorted_img, distorted_mask = distorsion_generator.sin(
+            distorted_img = distorsion_generator.sin(
                 rotated_img,
-                rotated_mask,
                 vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
-                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2)
             )
         elif distorsion_type == 2:
-            distorted_img, distorted_mask = distorsion_generator.cos(
+            distorted_img = distorsion_generator.cos(
                 rotated_img,
-                rotated_mask,
                 vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
-                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2)
             )
         else:
-            distorted_img, distorted_mask = distorsion_generator.random(
+            distorted_img = distorsion_generator.random(
                 rotated_img,
-                rotated_mask,
                 vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
-                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2)
             )
 
         ##################################
@@ -119,28 +73,14 @@ class FakeTextDataGenerator(object):
 
         # Horizontal text
         if orientation == 0:
-            new_width = int(
-                distorted_img.size[0]
-                * (float(size - vertical_margin) / float(distorted_img.size[1]))
-            )
-            resized_img = distorted_img.resize(
-                (new_width, size - vertical_margin), Image.ANTIALIAS
-            )
-            resized_mask = distorted_mask.resize((new_width, size - vertical_margin))
+            new_width = int(distorted_img.size[0] * (float(size - vertical_margin) / float(distorted_img.size[1])))
+            resized_img = distorted_img.resize((new_width, size - vertical_margin), Image.ANTIALIAS)
             background_width = width if width > 0 else new_width + horizontal_margin
             background_height = size
         # Vertical text
         elif orientation == 1:
-            new_height = int(
-                float(distorted_img.size[1])
-                * (float(size - horizontal_margin) / float(distorted_img.size[0]))
-            )
-            resized_img = distorted_img.resize(
-                (size - horizontal_margin, new_height), Image.ANTIALIAS
-            )
-            resized_mask = distorted_mask.resize(
-                (size - horizontal_margin, new_height), Image.ANTIALIAS
-            )
+            new_height = int(float(distorted_img.size[1]) * (float(size - horizontal_margin) / float(distorted_img.size[0])))
+            resized_img = distorted_img.resize((size - horizontal_margin, new_height), Image.ANTIALIAS)
             background_width = size
             background_height = new_height + vertical_margin
         else:
@@ -150,24 +90,13 @@ class FakeTextDataGenerator(object):
         # Generate background image #
         #############################
         if background_type == 0:
-            background_img = background_generator.gaussian_noise(
-                background_height, background_width
-            )
+            background = background_generator.gaussian_noise(background_height, background_width)
         elif background_type == 1:
-            background_img = background_generator.plain_white(
-                background_height, background_width
-            )
+            background = background_generator.plain_white(background_height, background_width)
         elif background_type == 2:
-            background_img = background_generator.quasicrystal(
-                background_height, background_width
-            )
+            background = background_generator.quasicrystal(background_height, background_width)
         else:
-            background_img = background_generator.image(
-                background_height, background_width, image_dir
-            )
-        background_mask = Image.new(
-            "RGB", (background_width, background_height), (0, 0, 0)
-        )
+            background = background_generator.picture(background_height, background_width)
 
         #############################
         # Place text with alignment #
@@ -176,62 +105,34 @@ class FakeTextDataGenerator(object):
         new_text_width, _ = resized_img.size
 
         if alignment == 0 or width == -1:
-            background_img.paste(resized_img, (margin_left, margin_top), resized_img)
-            background_mask.paste(resized_mask, (margin_left, margin_top))
+            background.paste(resized_img, (margin_left, margin_top), resized_img)
         elif alignment == 1:
-            background_img.paste(
-                resized_img,
-                (int(background_width / 2 - new_text_width / 2), margin_top),
-                resized_img,
-            )
-            background_mask.paste(
-                resized_mask,
-                (int(background_width / 2 - new_text_width / 2), margin_top),
-            )
+            background.paste(resized_img, (int(background_width / 2 - new_text_width / 2), margin_top), resized_img)
         else:
-            background_img.paste(
-                resized_img,
-                (background_width - new_text_width - margin_right, margin_top),
-                resized_img,
-            )
-            background_mask.paste(
-                resized_mask,
-                (background_width - new_text_width - margin_right, margin_top),
-            )
+            background.paste(resized_img, (background_width - new_text_width - margin_right, margin_top), resized_img)
 
         ##################################
         # Apply gaussian blur #
         ##################################
 
-        gaussian_filter = ImageFilter.GaussianBlur(
-            radius=blur if not random_blur else rnd.randint(0, blur)
+        final_image = background.filter(
+            ImageFilter.GaussianBlur(
+                radius=(blur if not random_blur else rnd.randint(0, blur))
+            )
         )
-        final_image = background_img.filter(gaussian_filter)
-        final_mask = background_mask.filter(gaussian_filter)
 
         #####################################
         # Generate name for resulting image #
         #####################################
         if name_format == 0:
-            image_name = "{}_{}.{}".format(text, str(index), extension)
-            mask_name = "{}_{}_mask.png".format(text, str(index))
+            image_name = '{}_{}.{}'.format(text, str(index), extension)
         elif name_format == 1:
-            image_name = "{}_{}.{}".format(str(index), text, extension)
-            mask_name = "{}_{}_mask.png".format(str(index), text)
+            image_name = '{}_{}.{}'.format(str(index), text, extension)
         elif name_format == 2:
-            image_name = "{}.{}".format(str(index), extension)
-            mask_name = "{}_mask.png".format(str(index))
+            image_name = '{}.{}'.format(str(index),extension)
         else:
-            print("{} is not a valid name format. Using default.".format(name_format))
-            image_name = "{}_{}.{}".format(text, str(index), extension)
-            mask_name = "{}_{}_mask.png".format(text, str(index))
+            print('{} is not a valid name format. Using default.'.format(name_format))
+            image_name = '{}_{}.{}'.format(text, str(index), extension)
 
         # Save the image
-        if out_dir is not None:
-            final_image.convert("RGB").save(os.path.join(out_dir, image_name))
-            if output_mask == 1:
-                final_mask.convert("RGB").save(os.path.join(out_dir, mask_name))
-        else:
-            if output_mask == 1:
-                return final_image.convert("RGB"), final_mask.convert("RGB")
-            return final_image.convert("RGB")
+        final_image.convert('RGB').save(os.path.join(out_dir, image_name))
--- OCR/deep-text-recognition-benchmark/data/generator/TextRecognitionDataGenerator/run.py
+++ OCR/deep-text-recognition-benchmark/data/generator/TextRecognitionDataGenerator/run.py
@@ -1,42 +1,37 @@
 import argparse
 import os, errno
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import random as rnd
 import string
 import sys
 
 from tqdm import tqdm
-from trdg.string_generator import (
+from string_generator import (
     create_strings_from_dict,
     create_strings_from_file,
     create_strings_from_wikipedia,
-    create_strings_randomly,
+    create_strings_randomly
 )
-from trdg.utils import load_dict, load_fonts
-from trdg.data_generator import FakeTextDataGenerator
+from data_generator import FakeTextDataGenerator
 from multiprocessing import Pool
 
-
 def margins(margin):
-    margins = margin.split(",")
+    margins = margin.split(',')
     if len(margins) == 1:
-        return [int(margins[0])] * 4
+        return [margins[0]] * 4
     return [int(m) for m in margins]
-
 
 def parse_arguments():
     """
         Parse the command line arguments of the program.
     """
 
-    parser = argparse.ArgumentParser(
-        description="Generate synthetic text data for text recognition."
-    )
+    parser = argparse.ArgumentParser(description='Generate synthetic text data for text recognition.')
     parser.add_argument(
-        "--output_dir", type=str, nargs="?", help="The output directory", default="out/"
+        "--output_dir",
+        type=str,
+        nargs="?",
+        help="The output directory",
+        default="out/",
     )
     parser.add_argument(
         "-i",
@@ -44,15 +39,15 @@ def parse_arguments():
         type=str,
         nargs="?",
         help="When set, this argument uses a specified text file as source for the text",
-        default="",
+        default=""
     )
     parser.add_argument(
         "-l",
         "--language",
         type=str,
         nargs="?",
-        help="The language to use, should be fr (French), en (English), es (Spanish), de (German), ar (Arabic), cn (Chinese), or hi (Hindi)",
-        default="en",
+        help="The language to use, should be fr (French), en (English), es (Spanish), de (German), or cn (Chinese).",
+        default="en"
     )
     parser.add_argument(
         "-c",
@@ -60,35 +55,35 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="The number of images to be created.",
-        required=True,
+        default=1000
     )
     parser.add_argument(
         "-rs",
         "--random_sequences",
         action="store_true",
         help="Use random sequences as the source text for the generation. Set '-let','-num','-sym' to use letters/numbers/symbols. If none specified, using all three.",
-        default=False,
+        default=False
     )
     parser.add_argument(
         "-let",
         "--include_letters",
         action="store_true",
         help="Define if random sequences should contain letters. Only works with -rs",
-        default=False,
+        default=False
     )
     parser.add_argument(
         "-num",
         "--include_numbers",
         action="store_true",
         help="Define if random sequences should contain numbers. Only works with -rs",
-        default=False,
+        default=False
     )
     parser.add_argument(
         "-sym",
         "--include_symbols",
         action="store_true",
         help="Define if random sequences should contain symbols. Only works with -rs",
-        default=False,
+        default=False
     )
     parser.add_argument(
         "-w",
@@ -96,14 +91,14 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="Define how many words should be included in each generated sample. If the text source is Wikipedia, this is the MINIMUM length",
-        default=1,
+        default=1
     )
     parser.add_argument(
         "-r",
         "--random",
         action="store_true",
         help="Define if the produced string will have variable word count (with --length being the maximum)",
-        default=False,
+        default=False
     )
     parser.add_argument(
         "-f",
@@ -171,14 +166,14 @@ def parse_arguments():
         "--background",
         type=int,
         nargs="?",
-        help="Define what kind of background to use. 0: Gaussian Noise, 1: Plain white, 2: Quasicrystal, 3: Image",
+        help="Define what kind of background to use. 0: Gaussian Noise, 1: Plain white, 2: Quasicrystal, 3: Pictures",
         default=0,
     )
     parser.add_argument(
         "-hw",
         "--handwritten",
         action="store_true",
-        help='Define if the data will be "handwritten" by an RNN',
+        help="Define if the data will be \"handwritten\" by an RNN",
     )
     parser.add_argument(
         "-na",
@@ -188,19 +183,12 @@ def parse_arguments():
         default=0,
     )
     parser.add_argument(
-        "-om",
-        "--output_mask",
-        type=int,
-        help="Define if the generator will return masks for the text",
-        default=0,
-    )
-    parser.add_argument(
         "-d",
         "--distorsion",
         type=int,
         nargs="?",
         help="Define a distorsion applied to the resulting image. 0: None (Default), 1: Sine wave, 2: Cosine wave, 3: Random",
-        default=0,
+        default=0
     )
     parser.add_argument(
         "-do",
@@ -208,7 +196,7 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="Define the distorsion's orientation. Only used if -d is specified. 0: Vertical (Up and down), 1: Horizontal (Left and Right), 2: Both",
-        default=0,
+        default=0
     )
     parser.add_argument(
         "-wd",
@@ -216,7 +204,7 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="Define the width of the resulting image. If not set it will be the width of the text + 10. If the width of the generated text is bigger that number will be used",
-        default=-1,
+        default=-1
     )
     parser.add_argument(
         "-al",
@@ -224,7 +212,7 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="Define the alignment of the text in the image. Only used if the width parameter is set. 0: left, 1: center, 2: right",
-        default=1,
+        default=1
     )
     parser.add_argument(
         "-or",
@@ -232,7 +220,7 @@ def parse_arguments():
         type=int,
         nargs="?",
         help="Define the orientation of the text. 0: Horizontal, 1: Vertical",
-        default=0,
+        default=0
     )
     parser.add_argument(
         "-tc",
@@ -240,7 +228,7 @@ def parse_arguments():
         type=str,
         nargs="?",
         help="Define the text's color, should be either a single hex color or a range in the ?,? format.",
-        default="#282828",
+        default='#282828'
     )
     parser.add_argument(
         "-sw",
@@ -248,15 +236,7 @@ def parse_arguments():
         type=float,
         nargs="?",
         help="Define the width of the spaces between words. 2.0 means twice the normal space width",
-        default=1.0,
-    )
-    parser.add_argument(
-        "-cs",
-        "--character_spacing",
-        type=int,
-        nargs="?",
-        help="Define the width of the spaces between characters. 2 means two pixels",
-        default=0,
+        default=1.0
     )
     parser.add_argument(
         "-m",
@@ -264,52 +244,47 @@ def parse_arguments():
         type=margins,
         nargs="?",
         help="Define the margins around the text when rendered. In pixels",
-        default=(5, 5, 5, 5),
+        default=(5, 5, 5, 5)
     )
     parser.add_argument(
         "-fi",
         "--fit",
         action="store_true",
         help="Apply a tight crop around the rendered text",
-        default=False,
+        default=False
     )
     parser.add_argument(
-        "-ft", "--font", type=str, nargs="?", help="Define font to be used"
-    )
-    parser.add_argument(
-        "-fd",
-        "--font_dir",
+        "-ft",
+        "--font",
         type=str,
         nargs="?",
-        help="Define a font directory to be used",
+        help="Define font to be used"
     )
-    parser.add_argument(
-        "-id",
-        "--image_dir",
-        type=str,
-        nargs="?",
-        help="Define an image directory to use when background is set to image",
-        default=os.path.join(os.path.split(os.path.realpath(__file__))[0], "images"),
-    )
-    parser.add_argument(
-        "-ca",
-        "--case",
-        type=str,
-        nargs="?",
-        help="Generate upper or lowercase only. arguments: upper or lower. Example: --case upper",
-    )
-    parser.add_argument(
-        "-dt", "--dict", type=str, nargs="?", help="Define the dictionary to be used"
-    )
-    parser.add_argument(
-        "-ws",
-        "--word_split",
-        action="store_true",
-        help="Split on words instead of on characters (preserves ligatures, no character spacing)",
-        default=False,
-    )
+
+
     return parser.parse_args()
 
+def load_dict(lang):
+    """
+        Read the dictionnary file and returns all words in it.
+    """
+
+    lang_dict = []
+    with open(os.path.join('dicts', lang + '.txt'), 'r', encoding="utf8", errors='ignore') as d:
+        lang_dict = [l for l in d.read().splitlines() if len(l) > 0]
+    return lang_dict
+
+def load_fonts(lang):
+    """
+        Load all fonts in the fonts directories
+    """
+
+    if lang == 'cn':
+        return [os.path.join('fonts/cn', font) for font in os.listdir('fonts/cn')]
+    elif lang == 'ko':
+        return [os.path.join('fonts/ko', font) for font in os.listdir('fonts/ko')]
+    else:
+        return [os.path.join('fonts/latin', font) for font in os.listdir('fonts/latin')]
 
 def main():
     """
@@ -327,122 +302,73 @@ def main():
             raise
 
     # Creating word list
-    if args.dict:
-        lang_dict = []
-        if os.path.isfile(args.dict):
-            with open(args.dict, "r", encoding="utf8", errors="ignore") as d:
-                lang_dict = [l for l in d.read().splitlines() if len(l) > 0]
-        else:
-            sys.exit("Cannot open dict")
-    else:
-        lang_dict = load_dict(args.language)
+    lang_dict = load_dict(args.language)
 
     # Create font (path) list
-    if args.font_dir:
-        fonts = [
-            os.path.join(args.font_dir, p)
-            for p in os.listdir(args.font_dir)
-            if os.path.splitext(p)[1] == ".ttf"
-        ]
-    elif args.font:
+    if not args.font:
+        fonts = load_fonts(args.language)
+    else:
         if os.path.isfile(args.font):
             fonts = [args.font]
         else:
             sys.exit("Cannot open font")
-    else:
-        fonts = load_fonts(args.language)
 
     # Creating synthetic sentences (or word)
     strings = []
 
     if args.use_wikipedia:
         strings = create_strings_from_wikipedia(args.length, args.count, args.language)
-    elif args.input_file != "":
+    elif args.input_file != '':
         strings = create_strings_from_file(args.input_file, args.count)
     elif args.random_sequences:
-        strings = create_strings_randomly(
-            args.length,
-            args.random,
-            args.count,
-            args.include_letters,
-            args.include_numbers,
-            args.include_symbols,
-            args.language,
-        )
+        strings = create_strings_randomly(args.length, args.random, args.count,
+                                          args.include_letters, args.include_numbers, args.include_symbols, args.language)
         # Set a name format compatible with special characters automatically if they are used
-        if args.include_symbols or True not in (
-            args.include_letters,
-            args.include_numbers,
-            args.include_symbols,
-        ):
+        if args.include_symbols or True not in (args.include_letters, args.include_numbers, args.include_symbols):
             args.name_format = 2
     else:
-        strings = create_strings_from_dict(
-            args.length, args.random, args.count, lang_dict
-        )
+        strings = create_strings_from_dict(args.length, args.random, args.count, lang_dict)
 
-    if args.language == "ar":
-        from arabic_reshaper import ArabicReshaper
-
-        arabic_reshaper = ArabicReshaper()
-        strings = [
-            " ".join([arabic_reshaper.reshape(w) for w in s.split(" ")[::-1]])
-            for s in strings
-        ]
-    if args.case == "upper":
-        strings = [x.upper() for x in strings]
-    if args.case == "lower":
-        strings = [x.lower() for x in strings]
 
     string_count = len(strings)
 
     p = Pool(args.thread_count)
-    for _ in tqdm(
-        p.imap_unordered(
-            FakeTextDataGenerator.generate_from_tuple,
-            zip(
-                [i for i in range(0, string_count)],
-                strings,
-                [fonts[rnd.randrange(0, len(fonts))] for _ in range(0, string_count)],
-                [args.output_dir] * string_count,
-                [args.format] * string_count,
-                [args.extension] * string_count,
-                [args.skew_angle] * string_count,
-                [args.random_skew] * string_count,
-                [args.blur] * string_count,
-                [args.random_blur] * string_count,
-                [args.background] * string_count,
-                [args.distorsion] * string_count,
-                [args.distorsion_orientation] * string_count,
-                [args.handwritten] * string_count,
-                [args.name_format] * string_count,
-                [args.width] * string_count,
-                [args.alignment] * string_count,
-                [args.text_color] * string_count,
-                [args.orientation] * string_count,
-                [args.space_width] * string_count,
-                [args.character_spacing] * string_count,
-                [args.margins] * string_count,
-                [args.fit] * string_count,
-                [args.output_mask] * string_count,
-                [args.word_split] * string_count,
-                [args.image_dir] * string_count,
-            ),
-        ),
-        total=args.count,
-    ):
+    for _ in tqdm(p.imap_unordered(
+        FakeTextDataGenerator.generate_from_tuple,
+        zip(
+            [i for i in range(0, string_count)],
+            strings,
+            [fonts[rnd.randrange(0, len(fonts))] for _ in range(0, string_count)],
+            [args.output_dir] * string_count,
+            [args.format] * string_count,
+            [args.extension] * string_count,
+            [args.skew_angle] * string_count,
+            [args.random_skew] * string_count,
+            [args.blur] * string_count,
+            [args.random_blur] * string_count,
+            [args.background] * string_count,
+            [args.distorsion] * string_count,
+            [args.distorsion_orientation] * string_count,
+            [args.handwritten] * string_count,
+            [args.name_format] * string_count,
+            [args.width] * string_count,
+            [args.alignment] * string_count,
+            [args.text_color] * string_count,
+            [args.orientation] * string_count,
+            [args.space_width] * string_count,
+            [args.margins] * string_count,
+            [args.fit] * string_count
+        )
+    ), total=args.count):
         pass
     p.terminate()
 
     if args.name_format == 2:
         # Create file with filename-to-label connections
-        with open(
-            os.path.join(args.output_dir, "labels.txt"), "w", encoding="utf8"
-        ) as f:
+        with open(os.path.join(args.output_dir, "labels.txt"), 'w', encoding="utf8") as f:
             for i in range(string_count):
-                file_name = strings[i] + "_" + str(i) + "." + args.extension
-                f.write("{}\t{}\n".format(file_name, strings[i]))
+                file_name = str(i) + "." + args.extension
+                f.write("{} {}\n".format(file_name, strings[i]))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
