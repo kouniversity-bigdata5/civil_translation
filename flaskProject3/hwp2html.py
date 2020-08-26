00b8991300a2842a83a32089c6b053cdb761585d
Joo-SangWon <vercv123@naver.com>
2020-08-26 23:10:20 +0900

    amend output html directory

--- flaskProject3/hwp2html.py
+++ flaskProject3/hwp2html.py
@@ -1,3 +1,5 @@
+import os
+
 path = 'C:/Users/JSW/PycharmProjects/flaskProject3/upload/'
 exefile = 'hwp5html'
 output = 'C:/Users/JSW/PycharmProjects/flaskProject3/file2html/'
@@ -7,11 +9,12 @@ def hwp2html(path):
    for root, dirs, files in os.walk(path):
       rootpath = os.path.join(os.path.abspath(path), root)
       for file in files:
+         filename = file.split('.')[0]
          filepath = os.path.join(rootpath, file)
          res.append(filepath)
       for result in res:
          result = '"' + result + '"'
-         os.system(exefile + ' ' + result + ' '+ output)
+         os.system(exefile+' '+result+' --output '+output+'/'+filename)
 
 
 if __name__ == "__main__":
